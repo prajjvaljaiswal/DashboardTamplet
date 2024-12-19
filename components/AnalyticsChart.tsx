e70f04462d35addc3e9affc8ae8bc2aab8b516f0
prajjvaljaiswal <prajjaiswal158@gmail.com>
2024-12-19 17:22:20 +0530

    - Analytic Chart

---
+++ components/AnalyticsChart.tsx
@@ -0,0 +1,42 @@
+'use client';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  ResponsiveContainer,
+} from "recharts";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+
+import data from '@/data/analtics'
+import { Monoton } from "next/font/google";
+
+const AnalyticsChart = () => {
+  return (
+  <>
+    <Card>
+      <CardHeader>Analycis Chart</CardHeader>
+      <CardContent>
+        <div style={{width:'100%', height: 300}}>
+          <ResponsiveContainer>
+            <LineChart width={1100} height={300} data={data}>
+                <Line type={"monotone"} dataKey={'uv'} stroke="#8884d8"/>
+                <CartesianGrid stroke="#ccc"/>
+                <XAxis dataKey={'name'}/>
+                <YAxis/>
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+      </CardContent>
+    </Card>
+  </>);
+};
+
+export default AnalyticsChart;
---
+++ types/analytics.ts
@@ -0,0 +1,6 @@
+export interface AnalyticsItem{
+    name: String;
+    uv: number;
+    pv: number;
+    amt: number;
+}
